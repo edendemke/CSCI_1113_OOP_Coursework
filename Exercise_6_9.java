92c3d55e1072b6deed332102e0ae78cb8e272194
Eden Demke <eden.adams123@gmail.com>
2023-12-19 14:30:33 -0700

    added exercise 6-9

---
+++ Exercise_6_9.java
@@ -0,0 +1,30 @@
+
+public class Exercise_6_9 {
+
+	public static void main(String[] args) {
+		double feet = 1.0;
+		double meter = 20.0;
+		
+		System.out.println("Feet    Meters          Meters  Feet   ");
+		System.out.println("---------------------------------------");
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.printf("%1.1f\t%1.3f\t\t%1.1f\t"
+					+ "%1.3f", feet, footToMeter(feet), meter, meterToFoot(meter));
+			System.out.println("");
+			
+			feet += 1;
+			meter += 5;
+		}
+	}
+	/** Convert from feet to meters */
+	public static double footToMeter(double foot) {
+		double meter = 0.305 * foot;
+		return meter;
+	}
+	/** Convert from meters to feet */
+	public static double meterToFoot(double meter) {
+		double foot = 3.279 * meter;
+		return foot;
+	}
+}
