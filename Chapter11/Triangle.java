d27b4d67287b6da2b665e75cd26a18a6dc49e116
Eden Demke <eden.adams123@gmail.com>
2024-01-18 14:53:47 -0700

    updated Triangle.java and added UML diagrams

--- Chapter11/Triangle.java
+++ Chapter11/Triangle.java
@@ -6,9 +6,11 @@ public class Triangle extends GeometricObject {
 	private double side3 = 1.0;
 	
 	public Triangle() {
+		super();
 	}
 	
 	public Triangle(double side1, double side2, double side3) {
+		super();
 		this.side1 = side1;
 		this.side2 = side2;
 		this.side3 = side3;
